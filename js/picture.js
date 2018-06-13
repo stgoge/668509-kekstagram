6f2348a7078b00c555a73b2937ecf69326a59386
Герман <stgoge@ya.ru>
2018-06-13 20:40:53 +0700

    3 задача

---
+++ js/picture.js
@@ -0,0 +1,92 @@
+'use strict';
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var DESCRIPTION = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
+var PAGES_NEEDED = 25;
+var IMG_PROPERTIES = {
+  src: 'photos/',
+  extension: '.jpg'
+};
+var COMMENTS_ON_PAGE = 2;
+var getRandomArrayElement = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+var generatePost = function (count) {
+  var post = {
+    comments: []
+  };
+  post.url = IMG_PROPERTIES.src + count + IMG_PROPERTIES.extension;
+  post.likes = Math.round(Math.max(Math.random() * 200, 15));
+  post.comments.push(getRandomArrayElement(COMMENTS));
+  if (Math.round(Math.random())) {
+    post.comments.push(getRandomArrayElement(COMMENTS));
+  }
+  post.description = getRandomArrayElement(DESCRIPTION);
+  return post;
+};
+var generatePosts = function (count) {
+  var posts = [];
+  for (var i = 1; i <= count; i++) {
+    posts.push(generatePost(i));
+  }
+  return posts;
+};
+var renderPostPreview = function (post) {
+  var postTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  var renderedPost = postTemplate.cloneNode(true);
+  renderedPost.querySelector('.picture__img').src = post.url;
+  renderedPost.querySelector('.picture__stat--likes').textContent = post.likes;
+  renderedPost.querySelector('.picture__stat--comments').textContent = post.comments.length;
+  return renderedPost;
+};
+var renderPreviewPage = function (posts) {
+  var previewPage = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < PAGES_NEEDED; i++) {
+    fragment.appendChild(renderPostPreview(posts[i]));
+  }
+  previewPage.appendChild(fragment);
+
+};
+var showBigPicture = function (post) {
+  var bigPicture = document.querySelector('.big-picture');
+  var bigPictureClone = bigPicture.cloneNode(true);
+  bigPictureClone.classList.remove('hidden');
+  bigPictureClone.querySelector('.big-picture__img img').src = post.url;
+  bigPictureClone.querySelector('.likes-count').textContent = post.likes;
+  bigPictureClone.querySelector('.comments-count').textContent = post.comments.length;
+  var comments = bigPictureClone.querySelectorAll('.social__comment');
+  for (var i = 0; i < COMMENTS_ON_PAGE; i++) {
+    if (post.comments[i]) {
+      comments[i].querySelector('.social__picture').src = 'img/avatar-' + Math.ceil(Math.random() * 5) + '.svg';
+      comments[i].querySelector('.social__text').textContent = post.comments[i];
+    } else {
+      comments[i].classList.add('visually-hidden');
+    }
+  }
+  bigPictureClone.querySelector('.social__caption').textContent = post.description;
+  bigPicture.parentNode.replaceChild(bigPictureClone, bigPicture);
+  document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  document.querySelector('.social__loadmore').classList.add('visually-hidden');
+};
+var showPictures = function () {
+  var posts = generatePosts(PAGES_NEEDED);
+  renderPreviewPage(posts);
+  showBigPicture(posts[0]);
+};
+
+
+showPictures();
