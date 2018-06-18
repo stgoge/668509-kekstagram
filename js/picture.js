81d9a66721af2c59f899ce94d9842e7381d31215
Герман <stgoge@ya.ru>
2018-06-18 09:51:54 +0700

    открытие bigPicture

--- js/picture.js
+++ js/picture.js
@@ -72,25 +72,29 @@ var IMAGE_STYLE_PROPERTY = {
 var getRandomIntFromRange = function (min, max) {
   return (Math.round((Math.random() * (max - min))) + min);
 };
+
 var getRandomListElement = function (list) {
   return list[getRandomIntFromRange(0, list.length - 1)];
 };
+
 var getPictureUrl = function (number) {
   return IMG_PROPERTY.path + number + IMG_PROPERTY.extension;
 };
+
 var generatePost = function (count) {
   var post = {
     url: getPictureUrl(count),
     likes: getRandomIntFromRange(LIKES_PROPERTY.min, LIKES_PROPERTY.max),
     DESCRIPTIONS: getRandomListElement(DESCRIPTIONS),
-    comments: []
-  };
+    comments: [],
+    id: count + '__preview'};
   var commentsCount = getRandomIntFromRange(COMMENTS_PROPERTY.min, COMMENTS_PROPERTY.max);
   for (var i = 0; i < commentsCount; i++) {
     post.comments.push(getRandomListElement(COMMENTS));
   }
   return post;
 };
+
 var generatePosts = function (count) {
   var posts = [];
   for (var i = 1; i <= count; i++) {
@@ -98,19 +102,23 @@ var generatePosts = function (count) {
   }
   return posts;
 };
+
 var renderPostPreview = function (post) {
   var renderedPost = document.querySelector('#picture').content.querySelector('.picture__link').cloneNode(true);
   renderedPost.querySelector('.picture__img').src = post.url;
+  renderedPost.querySelector('.picture__img').id = post.id;
   renderedPost.querySelector('.picture__stat--likes').textContent = post.likes;
   renderedPost.querySelector('.picture__stat--comments').textContent = post.comments.length;
   return renderedPost;
 };
+
 var renderComment = function (path, text) {
   var renderedComment = document.querySelector('#comment').content.querySelector('.social__comment').cloneNode(true);
   renderedComment.querySelector('img').src = path;
   renderedComment.querySelector('p').textContent = text;
   return renderedComment;
 };
+
 var renderComments = function (post) {
   var comments = document.createDocumentFragment();
   var commentUrl;
@@ -120,6 +128,7 @@ var renderComments = function (post) {
   }
   return comments;
 };
+
 var renderPreviewPage = function (posts) {
   var previewPage = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
@@ -128,8 +137,11 @@ var renderPreviewPage = function (posts) {
   }
   previewPage.appendChild(fragment);
 };
+var bigPicture;
+var bigPictureCancel;
+
 var renderBigPicture = function (post) {
-  var bigPicture = document.querySelector('.big-picture');
+  bigPicture = document.querySelector('.big-picture');
   var bigPictureClone = bigPicture.cloneNode(true);
   bigPictureClone.classList.remove('hidden');
   bigPictureClone.querySelector('.big-picture__img img').src = post.url;
@@ -139,13 +151,42 @@ var renderBigPicture = function (post) {
   bigPictureClone.querySelector('.social__comments').appendChild(renderComments(post));
   bigPictureClone.querySelector('.social__caption').textContent = post.DESCRIPTIONS;
   bigPicture.parentNode.replaceChild(bigPictureClone, bigPicture);
+  bigPicture = document.querySelector('.big-picture');
   document.querySelector('.social__comment-count').classList.add('visually-hidden');
   document.querySelector('.social__loadmore').classList.add('visually-hidden');
 };
+
+var bigPictureCloseByEsc = function (eve) {
+  debugger;
+  if (eve.keyCode === ESC_KEYCODE) {
+    bigPictureClose();
+  }
+
+};
+
+var bigPictureClose = function () {
+  bigPicture.classList.add('hidden');
+  bigPictureCancel.removeEventListener('click', bigPictureClose);
+  document.removeEventListener('keydown', bigPictureCloseByEsc);
+
+};
+
+var documentClickHandler = function (eve, posts) {
+  if (eve.target.classList.value === 'picture__img') {
+    renderBigPicture(posts[parseInt(eve.target.id, 10) - 1]);
+    bigPictureCancel = document.querySelector('.big-picture__cancel');
+    bigPictureCancel.addEventListener('click', bigPictureClose);
+    document.addEventListener('keydown', bigPictureCloseByEsc);
+
+  }
+};
+
 var showPictures = function () {
   var posts = generatePosts(POST_COUNT);
   renderPreviewPage(posts);
-//  renderBigPicture(posts[0]);
+  document.addEventListener('click', function (eve) {
+    documentClickHandler(eve, posts);
+  });
 };
 showPictures();
 
